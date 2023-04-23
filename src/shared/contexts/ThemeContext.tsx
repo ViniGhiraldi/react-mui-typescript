bb7507f82921b9b4f83c6c0b7f60ac20eb1d8d15
ViniGhiraldi <vinighiraldi198@gmail.com>
2023-04-23 18:32:08 -0300

    feat: added dark theme and theme context

---
+++ src/shared/contexts/ThemeContext.tsx
@@ -0,0 +1,43 @@
+import { Box } from '@mui/system';
+import { ThemeProvider } from "@mui/material";
+import { createContext, useCallback, useContext, useMemo, useState } from "react";
+import { LightTheme, DarkTheme } from "./../themes";
+
+interface IThemeContextData{
+    themeName: 'light' | 'dark';
+    toggleTheme: () => void;
+}
+
+const ThemeContext = createContext({} as IThemeContextData);
+
+interface IThemeProviderProps{
+    children?: React.ReactNode
+}
+
+export const useAppThemeContext = () => {
+    return useContext(ThemeContext);
+}
+
+export const AppThemeProvider = ({children}: IThemeProviderProps) => {
+    const [themeName, setThemeName] = useState<'light' | 'dark'>('light');
+    const toggleTheme = useCallback(()=>{
+        /* themeName === 'light' ? setThemeName('dark') : setThemeName('light'); */
+        setThemeName(oldThemeName => oldThemeName === 'light' ? 'dark' : 'light');
+    }, []);
+
+    const theme = useMemo(() => {
+        if (themeName === 'light') return LightTheme;
+
+        return DarkTheme;
+    }, [themeName]);
+
+    return(
+        <ThemeContext.Provider value={{themeName, toggleTheme}}>
+            <ThemeProvider theme={theme}>
+                <Box width='100vw' height='100vh' bgcolor={theme.palette.background.default}>
+                    {children}
+                </Box>
+            </ThemeProvider>
+        </ThemeContext.Provider>
+    );
+}
