1a0cbd9d802196a39603b6188e49d334d64a5a42
ViniGhiraldi <vinighiraldi198@gmail.com>
2023-05-01 14:06:39 -0300

    feat: added page pessoas and useDebounce hook

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,7 +1,7 @@
 import { Routes, Route, Navigate } from 'react-router-dom';
 import { useDrawerContext } from '../shared/contexts';
 import { useEffect } from 'react';
-import { Dashboard } from '../pages';
+import { Dashboard, ListagemDePessoas } from '../pages';
 
 export const AppRoutes = () => {
     const { setDrawerOptions } = useDrawerContext();
@@ -12,6 +12,11 @@ export const AppRoutes = () => {
           label: 'Página inicial',
           icon: 'home',
           path: '/pagina-inicial'
+        },
+        {
+          label: 'Pessoas',
+          icon: 'people',
+          path: '/pessoas'
         }
       ])
     },[])
@@ -19,6 +24,11 @@ export const AppRoutes = () => {
     return (
       <Routes>
         <Route path="/pagina-inicial" element={<Dashboard/>}/>
+
+        <Route path="/pessoas" element={<ListagemDePessoas/>}/>
+
+        {/* <Route path="/pessoas/detalhe/:id" element={<Dashboard/>}/> */}
+
         <Route path="*" element={<Navigate to="/pagina-inicial" />} />
       </Routes>
     );
