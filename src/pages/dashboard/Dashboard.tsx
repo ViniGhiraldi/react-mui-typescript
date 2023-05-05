689cb403aa516e70c8d7f8502475e2474f831b17
ViniGhiraldi <vinighiraldi198@gmail.com>
2023-05-05 11:36:59 -0300

    feat: finished dashboard

--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -1,17 +1,94 @@
-import { FerramentasDeDetalhe } from "../../shared/components";
+import { useEffect, useState } from "react";
+import { Box, Card, CardContent, CircularProgress, Grid, Typography } from "@mui/material";
+
+import { PessoasService } from "../../shared/services/api/pessoas/PessoasService";
+import { CidadesService } from "../../shared/services/api/cidades/CidadesService";
+import { FerramentasDaListagem } from "../../shared/components";
 import { LayoutBaseDePagina } from "../../shared/layouts";
 
 export const Dashboard = () => {
-    return(
-        <LayoutBaseDePagina
-          titulo='Página inicial' 
-          barraDeFerramentas={(
-            <FerramentasDeDetalhe
-              mostrarBotaoSalvarEVoltar
-            />
-          )}
-        >
-            Testando
-        </LayoutBaseDePagina>
+  const [isLoadingPessoas, setIsLoadingPessoas] = useState(false);
+  const [isLoadingCidades, setIsLoadingCidades] = useState(false);
+  const [pessoasTotalCount, setPessoasTotalCount] = useState(0);
+  const [cidadesTotalCount, setCidadesTotalCount] = useState(0);
+
+  useEffect(()=>{
+    setIsLoadingPessoas(true);
+    setIsLoadingCidades(true);
+
+    PessoasService.getAll().then((result)=>{
+      setIsLoadingPessoas(false);
+      if(result instanceof Error){
+        alert(result.message);
+      }else{
+        setPessoasTotalCount(result.totalCount);
+      }
+    });
+    CidadesService.getAll().then((result) => {
+      setIsLoadingCidades(false);
+      if(result instanceof Error){
+        alert(result.message);
+      }else{
+        setCidadesTotalCount(result.totalCount);
+      }
+    })
+  }, [])
+
+    return (
+      <LayoutBaseDePagina
+        titulo="Página inicial"
+        barraDeFerramentas={<FerramentasDaListagem mostrarBotaoNovo={false} />}
+      >
+        <Box width="100%" display="flex">
+          <Grid container margin={2}>
+            <Grid item container spacing={2}>
+              <Grid item xs={12} md={6} lg={4} xl={3}>
+                <Card>
+                  <CardContent>
+                    <Typography variant="h5" align="center">
+                      Total de pessoas
+                    </Typography>
+
+                    <Box padding={6} display="flex" justifyContent="center" alignItems="center" >
+                      {!isLoadingPessoas && (
+                        <Typography variant="h1">
+                          {pessoasTotalCount}
+                        </Typography>
+                      )}
+                      {isLoadingPessoas && (
+                        <Typography variant='h1'>
+                          <CircularProgress size={70} />
+                        </Typography>
+                      )}
+                    </Box>
+                  </CardContent>
+                </Card>
+              </Grid>
+              <Grid item xs={12} md={6} lg={4} xl={3}>
+                <Card>
+                  <CardContent>
+                    <Typography variant="h5" align="center">
+                      Total de cidades
+                    </Typography>
+
+                    <Box padding={6} display="flex" justifyContent="center" alignItems="center" >
+                      {!isLoadingCidades && (
+                        <Typography variant="h1">
+                          {cidadesTotalCount}
+                          </Typography>
+                      )}
+                      {isLoadingCidades && (
+                        <Typography variant='h1'>
+                          <CircularProgress size={70} />
+                        </Typography>
+                      )}
+                    </Box>
+                  </CardContent>
+                </Card>
+              </Grid>
+            </Grid>
+          </Grid>
+        </Box>
+      </LayoutBaseDePagina>
     );
 };
